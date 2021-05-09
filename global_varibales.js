c2b541eed8cd7ecf1611c8cc2a580d2a89ec07a9
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2021-05-09 22:47:53 +0200

    moved configuration variables into global_variables.js

---
+++ global_varibales.js
@@ -0,0 +1,13 @@
+    var mode = 1;
+    var read = false;
+    var grid_status = true;
+    var objects = [];
+    var lines = [];
+    var griding = 20;
+    
+    var text_size = "10px";
+    var radius = 30;
+    var square_w = 80;
+    var square_h = 50;
+
+    var last_selected = 0;
