2497a040ec8d7083f766b412ea57415b0fd9e7ec
Maximilian Löffler <s8maloef@stud.uni-saarland.de>
2021-05-14 21:34:33 +0200

    fixed zooming + zoom amout for square changed

--- helpers.js
+++ helpers.js
@@ -369,15 +369,17 @@ let clear_canvas = function(clear_objects) {
  */
 function scale(operator) {
     if (operator == "+") {
-        griding  -= 5;
-        Circle.radius += 5;
-        Square.width += 5;
-        Square.height = Square.width * 0.6;
-        text_size = String(Number(text_size.replace('px', '')) + 1.5) + 'px';
+        if (griding > 5) {
+            griding  -= 5;
+            Circle.radius += 5;
+            Square.width += 12;
+            Square.height = Square.width * 0.6;
+            text_size = String(Number(text_size.replace('px', '')) + 1.5) + 'px';
+        }
     } else {
         griding  += 5;
         Circle.radius -= 5;
-        Square.width -= 5;
+        Square.width -= 12;
         Square.height = Square.width * 0.6;
         text_size = String(Number(text_size.replace('px', '')) - 1.5) + 'px';
     }
